78f98960052af9cf6617b2ca9697b89da2cf843e
Germán Santa Cruz <gdsantacruz@hotmail.com>
2026-02-25 21:12:23 -0400

    agregado varios fields al modelo book

--- addons/library_management/models/book.py
+++ addons/library_management/models/book.py
@@ -4,4 +4,14 @@ class LibraryBook(models.Model):
     _name = 'library.book'
     _description = 'Libro de Biblioteca'
 
-    name = fields.Char(string='Título', required=True)    
+    name = fields.Char(string='Título', required=True)   
+    isbn = fields.Char(string='ISBN')
+    fecha_publicacion = fields.Date(string='Fecha de publicación') 
+    descripcion = fields.Text(string='Descripcion')
+    price = fields.Float(string="Precio")
+    state = fields.Selection([
+        ('available', 'Disponible'),
+        ('borrowed', 'Prestado'),
+    ], default='available', string='Estado')
+    active = fields.Boolean(string='Activo', default=True,
+        help="Si está desmarcado, permite ocultar el libro sin eliminarlo de la base de datos.")
